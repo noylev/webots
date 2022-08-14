f7a63c55ea30f782fc2cc06f6e2271a08958e1fc
noylevi <noylevi91@gmail.com>
2022-08-14 16:34:28 +0300

    added Home component

---
+++ src/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div className="App">
+            Increase sales and keep you audiance hot
+            with Whatsapp and Telegram Chatbots
+
+        </div>
+    )
+}
+
+export default Home;
